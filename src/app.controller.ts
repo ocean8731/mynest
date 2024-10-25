ce09210a34b9034240b8ae10957eb7a031e3fe5d
Ocean <jeayeong70@gmail.com>
2024-10-26 04:15:33 +0900

    Feat : 컨트롤러 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,82 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Movie {
+  id: number;
+  title: string;
+}
+
+@Controller('movie')
 export class AppController {
+  private movies: Movie[] = [ {
+    id : 1,
+    title : "a",
+  }, {
+      id : 2,
+      title : "b",
+    }
+  ]
+  private idCounter = 3;
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Query : Query Parameter
+  getMovies(@Query('title')title: string){
+    if(!title) {
+      return this.movies;
+    }
+    return this.movies.filter( m => m.title.startsWith(title));
+  }
+  @Get(':id')
+  getMovie(@Param('id') id: string){
+    // movies에 param에 입력한 값과 같은 id가 있는가?
+    const movie = this.movies.find((m)=> m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 영화입니다.');
+    }
+
+    return movie;
+  }
+  @Post()
+  postMovie(
+    @Body('title') title: string,
+  ) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title: title
+    };
+
+    this.movies.push(
+      movie
+    )
+
+    return movie;
+  }
+  @Patch(':id')
+  patchMovie(
+    @Param('id') id: string,
+    @Body('title') title: string,
+    ) {
+    const movie = this.movies.find((m)=> m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 영화입니다.');
+    }
+    Object.assign(movie, {title});
+    return movie;
+  }
+  @Delete(':id')
+  deleteMovie(
+    @Param('id') id: string,
+  ){
+    const movieIndex = this.movies.findIndex((m)=> m.id === +id);
+
+    if (movieIndex === -1) {
+      throw new NotFoundException('존재하지 않는 영화입니다.');
+    }
+    this.movies.splice(movieIndex, 1)
+
+    return id;
   }
 }
