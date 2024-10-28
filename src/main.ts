d1a833df6b6e900d881552660b8325f78d264b74
Ocean <ocean@runners.im>
2024-10-28 20:34:01 +0900

    Feat : Password Validator 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 실제로 정의하지 않은 값은 들어가지 않게 하는 옵션
+      whitelist: true,
+      // 정의하지 않은 값이 들어갔을 때 에러를 발생시키는 옵션
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 
--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,4 +1,8 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateMovieDto {
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty() // message : genre should be no empty
   genre: string;
 }
