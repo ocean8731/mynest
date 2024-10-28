adba096e8e7dbe278b167b58ffa45e11cb3c0f4b
Ocean <jeayeong70@gmail.com>
2024-10-28 13:30:16 +0900

    Add : 설명용 주석

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 기준에 맞게 IOC 컨테이너가 인스턴스화
 @Module({
   imports: [],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+// IOC 컨테이너에게 어떤 클래스를 사용할지 알려주는 역할
 @Injectable()
 export class AppService {
   getHello(): string {
