454039dee88c6e46d2b5d43b0fcdcb4f664ef352
Jeayeong <yeong5678@naver.com>
2025-02-25 22:14:00 +0900

    feat : 환경변수 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 // 중앙 모듈 역할
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    // supabase 이용 시에는 session puller 이용
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      // 개발서버에서만 켜놓자!
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
 })
 export class AppModule {}
