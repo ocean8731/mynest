cf908ca7772a67c4ccaa2c952b6a4d207ab73a64
JeaYeong <yeong5678@naver.com>
2025-03-01 15:57:50 +0900

    Feat : 환경변수수 검증 기능 추가가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,53 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 // 중앙 모듈 역할
 @Module({
   imports: [
-    ConfigModule.forRoot({}),
-    // supabase 이용 시에는 session puller 이용
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [],
-      // 개발서버에서만 켜놓자!
-      synchronize: true,
+    ConfigModule.forRoot({
+      // 전역 모듈로 설정
+      isGlobal: true,
+      validationSchema: Joi.object({
+        // 환경변수 검증
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
     }),
+    // supabase 이용 시에는 session puller 이용
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [],
+        // 개발서버에서만 켜놓자!
+        synchronize: true,
+      }),
+      // IOC 컨테이너에 담길 객체를 지정
+      inject: [ConfigService],
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: process.env.DB_TYPE as 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [],
+    //   // 개발서버에서만 켜놓자!
+    //   synchronize: true,
+    // }),
     MoviesModule,
   ],
 })
