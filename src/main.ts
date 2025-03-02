ba8e9e2f64595a5f9fd810255a03d32238d47c34
JeaYeong <yeong5678@naver.com>
2025-03-02 12:57:24 +0900

    Feat : Swagger Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('NestJS API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  // Swagger 문서 생성
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); 
   // Validation
   app.useGlobalPipes(
     new ValidationPipe({
